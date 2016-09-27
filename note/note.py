27cef68a1ca888f6fc1f081309da24b145c9d496
Matthew Champion <mattjameschampion@gmail.com>
2016-09-27 19:18:47 +0100

    Add convert_note_to_abstract

--- note/note.py
+++ note/note.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+DEFAULT_NOTE_SHIFT_VALUE = "natural"
+
 
 class AbstractNote(Enum):
     bsharp_cnatural = 0
@@ -14,3 +16,38 @@ class AbstractNote(Enum):
     anatural = 9
     asharp_bflat = 10
     bnatural_cflat = 11
+
+
+def convert_note_to_abstract(note_letter, note_shift=DEFAULT_NOTE_SHIFT_VALUE):
+    note_translation_dict = {
+        ("b", "sharp"): AbstractNote.bsharp_cnatural,
+        ("c", "natural"): AbstractNote.bsharp_cnatural,
+        ("c", "sharp"): AbstractNote.csharp_dflat,
+        ("d", "flat"): AbstractNote.csharp_dflat,
+        ("d", "natural"): AbstractNote.dnatural,
+        ("d", "sharp"): AbstractNote.dsharp_eflat,
+        ("e", "flat"): AbstractNote.dsharp_eflat,
+        ("e", "natural"): AbstractNote.enatural_fflat,
+        ("f", "flat"): AbstractNote.enatural_fflat,
+        ("e", "sharp"): AbstractNote.esharp_fnatural,
+        ("f", "natural"): AbstractNote.esharp_fnatural,
+        ("f", "sharp"): AbstractNote.fsharp_gflat,
+        ("g", "flat"): AbstractNote.fsharp_gflat,
+        ("g", "natural"): AbstractNote.gnatural,
+        ("g", "sharp"): AbstractNote.gsharp_aflat,
+        ("a", "flat"): AbstractNote.gsharp_aflat,
+        ("a", "natural"): AbstractNote.anatural,
+        ("a", "sharp"): AbstractNote.asharp_bflat,
+        ("b", "flat"): AbstractNote.asharp_bflat,
+        ("b", "natural"): AbstractNote.bnatural_cflat,
+        ("c", "flat"): AbstractNote.bnatural_cflat
+    }
+    try:
+        return note_translation_dict[(note_letter.lower(), note_shift.lower())]
+    except AttributeError:
+        raise TypeError("note_letter ({}) and/or note_shift ({}) are "
+                        "an invalid type".format(note_letter, note_shift))
+    except KeyError:
+        raise ValueError("note_letter ({}) and/or note_shift ({}) "
+                         "do not match with a "
+                         "valid AbstractNote".format(note_letter, note_shift))
--- tests/note/test_note.py
+++ tests/note/test_note.py
@@ -1,5 +1,5 @@
 import unittest
-from note.note import AbstractNote
+from note.note import AbstractNote, convert_note_to_abstract
 
 
 class TestAbstractNote(unittest.TestCase):
@@ -30,3 +30,64 @@ class TestAbstractNote(unittest.TestCase):
         for invalid_input in invalid_inputs:
             with self.subTest(invalid_input=invalid_input):
                 self.assertRaises(ValueError, AbstractNote, invalid_input)
+
+
+class TestConvertNoteToAbstract(unittest.TestCase):
+    def test_does_not_throw_exception_with_valid_input(self):
+        argument_pairs = (
+            ("a", "flat"),
+            ("b", "natural"),
+            ("c", "sharp"),
+            ("d")
+        )
+
+        for argument_pair in argument_pairs:
+            with self.subTest(argument_pair=argument_pair):
+                try:
+                    convert_note_to_abstract(*argument_pair)
+                except Exception:
+                    self.fail("convert_note_to_abstract failed with "
+                              "valid input")
+
+    def test_returns_correct_result_on_valid_input(self):
+        argument_result_pairs = (
+            (("A", "FLAT"), AbstractNote.gsharp_aflat),
+            (("b", "natural"), AbstractNote.bnatural_cflat),
+            (("G", "Sharp"), AbstractNote.gsharp_aflat),
+            (("d"), AbstractNote.dnatural)
+        )
+
+        for argument_result_pair in argument_result_pairs:
+            argument, intended_result = argument_result_pair
+            with self.subTest(argument_result_pair=argument_result_pair):
+                actual_result = convert_note_to_abstract(*argument)
+
+                self.assertEqual(actual_result, intended_result)
+
+    def test_raises_exception_with_invalid_input_value(self):
+        invalid_inputs = (
+            ("H"),
+            ("z"),
+            ("m", "flat"),
+            ("a", "abcdefg")
+        )
+
+        for invalid_input in invalid_inputs:
+            with self.subTest(invalid_input=invalid_input):
+                self.assertRaises(ValueError,
+                                  convert_note_to_abstract,
+                                  *invalid_input)
+
+    def test_raises_exception_with_invalid_input_type(self):
+        invalid_inputs = (
+            -1,
+            0,
+            1,
+            100
+        )
+
+        for invalid_input in invalid_inputs:
+            with self.subTest(invalid_input=invalid_input):
+                self.assertRaises(TypeError,
+                                  convert_note_to_abstract,
+                                  invalid_input)
